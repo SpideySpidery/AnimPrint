ef64b3f428cb3fc6388520487441a5fdc6e71f3c
Spideyspidery <75521850+Spideyspidery@users.noreply.github.com>
2022-03-27 05:49:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import time
+
+def printy(text, delay=0.05):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+    
+#Thanks to Mortis666
